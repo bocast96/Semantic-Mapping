e80d3170628614d4d7000357df93141be9c1820a
Boris Castillo <bocast96@terpmail.umd.edu>
2016-12-12 10:13:29 -0500

    testing segmentation

--- segmentationTest.m
+++ segmentationTest.m
@@ -1,18 +1,62 @@
+% Segmenting object 000
 clear; clc;
 % update your path
-im = imread('C:\Users\Boris\Documents\School\CS 426 Image Processing\p5\Data\SingleObject\scene_001\frames\image_0_rgb.png');
-ic = imcrop(im, [150,150,300,250]);
-imshow(ic);
-I = rgb2gray(ic);
-imshow(I);
-%%
-background = imopen(I,strel('disk',30));
+path = 'Data\SingleObject\scene_000\';
+in = 'frames\';
+out = 'segmented_2d\';
+label = 'frame_';
+labEnd = '_rgb.png';
 
-% Display the Background Approximation as a Surface
-figure
-surf(double(background(1:8:end,1:8:end))),zlim([0 255]);
-ax = gca;
-ax.YDir = 'reverse';
+for i = 0:0
+    num = num2str(i);
+    im = imread(strcat(path, in, label, num, labEnd)); 
+    %imshow(im);
+    ic = imcrop(im, [150,140,300,250]);
+    %ic = rgb2gray(ic);
+    background = imopen(ic,strel('disk',30));
+    i_seg = ic - background;
+    outfile = strcat(path, out, 'seg_obj_', num, labEnd);
+    imwrite(i_seg, outfile);
+    imshow(i_seg);
+end
+
 %%
-I2 = I - background;
-imshow(I2)
+id = imread(strcat(path, in, label, '0_depth.png')); 
+id = imcrop(id, [150,140,300,250]);
+%%
+i_msk = rgb2gray(i_seg);
+i_msk = imbinarize(i_msk,0.2);
+imf = imfill(i_msk, 'holes');
+%imf = imopen(imf,strel('line', 5,2));
+imf = bwareafilt(imf,1);
+imshow(imf);
+
+%%
+id = im2double(id);
+imf = im2double(imf);
+id = id.*imf;
+imshow(id*100);
+
+%%
+ic = im2double(ic);
+im = ic.*repmat(imf,[1,1,3]);
+imshow(im);
+
+%%
+im = im2uint8(im);
+id = im2uint16(id);
+%%
+[pcx, pcy, pcz, r, g, b, D_, X, Y,validInd] = depthToCloud_full_RGB(id, im, 'params/calib_xtion.mat');
+%%
+Pts = [pcx pcy pcz];
+pcshow(Pts,[r g b]/255);
+drawnow;
+title('3D Point Cloud');
+
+
+
+
+
+
+
+
