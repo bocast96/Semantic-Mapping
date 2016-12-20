db7f52e407e3562e5e7819935625038c92eb101b
Boris Castillo <bocast96@terpmail.umd.edu>
2016-12-19 20:03:55 -0500

    second multiple object scene

---
+++ get_multObj_035.m
@@ -0,0 +1,43 @@
+function [point_clouds] = get_multObj_035(start, finish)
+    % update your path
+    path = 'Data\MultipleObjects\scene_035\frames\frame_';
+    len = finish - start + 1;
+    point_clouds = cell(len,2);
+    cur = start;
+    for idx = 1:len
+        num = num2str(cur);
+        i = imread(strcat(path, num, '_rgb.png'));
+        id = imread(strcat(path, num, '_depth.png'));
+        if cur < 390
+            x = 100;
+        else
+            x = 200;
+        end
+        
+        rec = [x, 0, 500, 400];
+        i = imcrop(i, rec);
+        id = imcrop(id, rec);
+        
+        [pcx, pcy, pcz, r, g, b, ~, ~, ~, ~] = depthToCloud_full_RGB(id, i, 'params/calib_xtion.mat');
+        Pts = [pcx pcy pcz];
+        rgb = [r g b]/255;
+        
+        %%
+        [Pts, rgb] = removeLargestPlain(Pts, rgb, 20, 20);
+        %pcshow(Pts, rgb);
+
+        %%
+        [Pts, rgb] = removeLargestPlain(Pts, rgb, 20, 20);
+        %pcshow(Pts, rgb);
+
+        %%
+        [Pts, rgb] = outlier_rejection(Pts, rgb, 300);
+        %pcshow(Pts, rgb);
+        
+        point_clouds{idx, 1} = Pts;
+        point_clouds{idx, 2} = rgb;
+        cur = cur+1;
+        clear Pts && rgb && i && id && pcx && pcy && pcz && r && g && b
+    end
+    
+end
